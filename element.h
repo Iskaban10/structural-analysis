7afe721095f768134a64fc7369edeb7c1ff2f075
Iskaban10 <janasourish197@gmail.com>
2025-03-19 21:15:25 +0530

    Memnber Class ans Node Class merged into Element Class

--- element.h
+++ element.h
@@ -21,4 +21,22 @@ public:
         cout << "y coordinate: " << y;
         cout << "z coordinate: " << z;
     }
-}
+};
+class Member
+{
+public:
+    string id;
+    Node *start, *end;
+    Member(int memberId, Node *n1, Node *n2)
+    {
+        id = memberId;
+        start = n1;
+        end = n2;
+    }
+    void display_details()
+    {
+        cout << "Member_Id: " << id;
+        cout << "Spans from " << start->id;
+        cout << "Spans to " << end->id;
+    }
+};
